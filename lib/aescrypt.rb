47b4fc1476558c58ae89c1ca44edb0f9f14ce908
Josh Sokol <jsokol@austin.rr.com>
2012-04-22 22:15:18 -0500

    Added AESCrypt library

---
+++ lib/aescrypt.rb
@@ -0,0 +1,54 @@
+#################################################
+# AESCrypt                                      #
+#-----------------------------------------------#
+# This library defines the functions to encrypt #
+# and decrypt files.                            #
+#################################################
+
+require 'openssl'
+
+module AESCrypt
+  # Decrypts a block of data (encrypted_data) given an encryption key
+  # and an initialization vector (iv).  Keys, iv's, and the data 
+  # returned are all binary strings.  Cipher_type should be
+  # "AES-256-CBC", "AES-256-ECB", or any of the cipher types
+  # supported by OpenSSL.  Pass nil for the iv if the encryption type
+  # doesn't use iv's (like ECB).
+  #:return: => String
+  #:arg: encrypted_data => String 
+  #:arg: key => String
+  #:arg: iv => String
+  #:arg: cipher_type => String
+  def AESCrypt.decrypt(encrypted_data, key, iv, cipher_type)
+    aes = OpenSSL::Cipher::Cipher.new(cipher_type)
+    aes.decrypt
+    aes.key = key
+    aes.iv = iv if iv != nil
+    aes.update(encrypted_data) + aes.final  
+  end
+  
+  # Encrypts a block of data given an encryption key and an 
+  # initialization vector (iv).  Keys, iv's, and the data returned 
+  # are all binary strings.  Cipher_type should be "AES-256-CBC",
+  # "AES-256-ECB", or any of the cipher types supported by OpenSSL.  
+  # Pass nil for the iv if the encryption type doesn't use iv's (like
+  # ECB).
+  #:return: => String
+  #:arg: data => String 
+  #:arg: key => String
+  #:arg: iv => String
+  #:arg: cipher_type => String  
+  def AESCrypt.encrypt(data, key, iv, cipher_type)
+    aes = OpenSSL::Cipher::Cipher.new(cipher_type)
+    aes.encrypt
+    aes.key = key
+    aes.iv = iv if iv != nil
+    aes.update(data) + aes.final      
+  end
+
+  # FUNCTION: Create a sufficient long encryption key off a provided password
+  def getKey(password)
+    key = OpenSSL::Digest::SHA512.new(@password).digest
+    return key
+  end
+end
